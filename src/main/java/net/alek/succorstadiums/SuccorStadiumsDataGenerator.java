ff5da85980a1d032c10352d8d98adb8930af88f6
JaydenGarrett <swatj2472@gmail.com>
2026-05-19 00:46:36 -0600

    Initial commit

---
+++ src/main/java/net/alek/succorstadiums/SuccorStadiumsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.alek.succorstadiums;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SuccorStadiumsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
