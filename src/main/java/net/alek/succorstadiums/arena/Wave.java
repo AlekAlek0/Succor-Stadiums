0402d19a3d73733ea61407382e2501019adac3ea
JaydenGarrett <swatj2472@gmail.com>
2026-05-21 19:36:44 -0600

    Added Features:
    MobArena System
    Wave System
    Fixed Bean Pole
    Succor Stadiums Trinkets Creative Mode Tab
    Succor Stadiums Food Creative Mode Tab

--- src/main/java/net/alek/succorstadiums/arena/Wave.java
+++ src/main/java/net/alek/succorstadiums/arena/Wave.java
@@ -1,9 +1,6 @@
 package net.alek.succorstadiums.arena;
 
-import net.minecraft.world.entity.Mob;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class Wave {
