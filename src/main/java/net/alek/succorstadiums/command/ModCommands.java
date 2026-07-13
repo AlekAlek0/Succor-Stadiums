110de78d46efce339feaed74a48aa2de8ef39233
JaydenGarrett <swatj2472@gmail.com>
2026-07-12 18:02:50 -0600

    Added Comments:
    ModCommands

--- src/main/java/net/alek/succorstadiums/command/ModCommands.java
+++ src/main/java/net/alek/succorstadiums/command/ModCommands.java
@@ -9,20 +9,22 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.server.level.ServerPlayer;
 
+// Mod commands class
 public class ModCommands {
 
+    // Register method for mod commands
     public static void registerModCommands() {
 
+        // Register the mob arena gui command
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
                 dispatcher.register(
                         Commands.literal("succorstadiums")
-
                                 .then(Commands.literal("mobarenaGUI").executes(ModCommands::OpenMobArenaGUI))
-
                 )
         );
     }
 
+    // Helper method for the open mob arena gui command
     private static int OpenMobArenaGUI(CommandContext<CommandSourceStack> ctx) {
         if (ctx.getSource().getEntity() instanceof ServerPlayer player) {
             ServerPlayNetworking.send(player, new OpenMobArenaPayload());
