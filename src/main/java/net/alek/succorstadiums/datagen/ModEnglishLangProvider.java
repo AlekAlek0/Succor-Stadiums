75814429253efd8f00c6d3a339e6d7050b72306e
JaydenGarrett <swatj2472@gmail.com>
2026-07-30 15:49:54 -0600

    Added Translations for Grass Creeper

--- src/main/java/net/alek/succorstadiums/datagen/ModEnglishLangProvider.java
+++ src/main/java/net/alek/succorstadiums/datagen/ModEnglishLangProvider.java
@@ -155,6 +155,7 @@ public class ModEnglishLangProvider extends FabricLanguageProvider {
         // Entity translations
         translationBuilder.add("entity.succorstadiums.banana_slime", "Banana Slime");
         translationBuilder.add("entity.succorstadiums.zombie_farmer", "Zombie Farmer");
+        translationBuilder.add("entity.succorstadiums.grass_creeper", "Grass Creeper");
 
         // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
