8e820ba8d3cc3c02b40bcb9876def02d086616e9
JaydenGarrett <swatj2472@gmail.com>
2026-08-04 17:11:03 -0600

    -Added 1 armor to arachno carapace armor pieces

--- src/main/java/net/alek/succorstadiums/item/armor/ArachnoCarapaceArmorItem.java
+++ src/main/java/net/alek/succorstadiums/item/armor/ArachnoCarapaceArmorItem.java
@@ -1,28 +1,36 @@
 package net.alek.succorstadiums.item.armor;
 
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.item.Item;
-import net.minecraft.resources.Identifier;
-
 import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.Item;
 
 // Arachno carapace armor class
 public class ArachnoCarapaceArmorItem extends Item {
 
-    // Define the step height, jump strength, and movement ids
-    private static final Identifier STEP_HEIGHT_ID = Identifier.withDefaultNamespace("arachno_step_height");
+    // Define the safe fall distance, step height, and armor ids
     private static final Identifier SAFE_FALL_DISTANCE_ID = Identifier.withDefaultNamespace("arachno_safe_fall_distance");
-
+    private static final Identifier STEP_HEIGHT_ID = Identifier.withDefaultNamespace("arachno_step_height");
+    private static final Identifier ARMOR_ID = Identifier.withDefaultNamespace("arachno_armor");
 
     public ArachnoCarapaceArmorItem(Item.Properties properties) {
         super(properties.component(
                 DataComponents.ATTRIBUTE_MODIFIERS,
-
-                // Modify the item attributes for step height by the added values given
+                // Modify the item attributes for safe fall distance, step height,
+                // and armor by the added values given
                 ItemAttributeModifiers.builder()
+                        .add(
+                                Attributes.SAFE_FALL_DISTANCE,
+                                new AttributeModifier(
+                                        SAFE_FALL_DISTANCE_ID,
+                                        1,
+                                        AttributeModifier.Operation.ADD_VALUE
+                                ),
+                                EquipmentSlotGroup.ARMOR
+                        )
                         .add(
                                 Attributes.STEP_HEIGHT,
                                 new AttributeModifier(
@@ -33,9 +41,9 @@ public class ArachnoCarapaceArmorItem extends Item {
                                 EquipmentSlotGroup.ARMOR
                         )
                         .add(
-                                Attributes.SAFE_FALL_DISTANCE,
+                                Attributes.ARMOR,
                                 new AttributeModifier(
-                                        SAFE_FALL_DISTANCE_ID,
+                                        ARMOR_ID,
                                         1,
                                         AttributeModifier.Operation.ADD_VALUE
                                 ),
