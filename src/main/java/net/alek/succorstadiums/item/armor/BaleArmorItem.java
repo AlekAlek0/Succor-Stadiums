c181de0eaabc8ce0104833b3c80894dbf853a9ee
JaydenGarrett <swatj2472@gmail.com>
2026-08-13 05:04:27 -0600

    -Updated durability for Bale Armor Set

--- src/main/java/net/alek/succorstadiums/item/armor/BaleArmorItem.java
+++ src/main/java/net/alek/succorstadiums/item/armor/BaleArmorItem.java
@@ -1,24 +1,42 @@
 package net.alek.succorstadiums.item.armor;
 
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.equipment.ArmorMaterial;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.item.enchantment.ItemEnchantments;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.equipment.ArmorMaterial;
 import net.minecraft.world.item.equipment.ArmorType;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+
+import org.jspecify.annotations.NonNull;
 
 public class BaleArmorItem extends Item {
     public BaleArmorItem(ArmorMaterial material, ArmorType type, Properties properties) {
-        super(applyBaleEnchants(type, properties.humanoidArmor(material, type)));
+        super(applyBaleEnchants(
+                type,
+                properties.humanoidArmor(material, type)
+                        .durability(getDurability(type))
+        ));
+    }
+
+    private static int getDurability(ArmorType type) {
+        return switch (type) {
+            case HELMET -> 242;
+            case CHESTPLATE -> 352;
+            case LEGGINGS -> 330;
+            case BOOTS -> 282;
+            default -> 0;
+        };
     }
 
     private static Properties applyBaleEnchants(ArmorType type, Properties properties) {
         return properties.delayedComponent(DataComponents.ENCHANTMENTS, (context) -> {
-            ItemEnchantments.Mutable mutable = new ItemEnchantments.Mutable(ItemEnchantments.EMPTY);
+            ItemEnchantments.Mutable mutable =
+                    new ItemEnchantments.Mutable(ItemEnchantments.EMPTY);
 
             switch (type) {
-                case HELMET, CHESTPLATE -> mutable.set(context.getOrThrow(Enchantments.PROJECTILE_PROTECTION), 2);
+                case HELMET, CHESTPLATE ->
+                        mutable.set(context.getOrThrow(Enchantments.PROJECTILE_PROTECTION), 2);
                 case LEGGINGS -> {
                     mutable.set(context.getOrThrow(Enchantments.SWIFT_SNEAK), 1);
                     mutable.set(context.getOrThrow(Enchantments.PROJECTILE_PROTECTION), 1);
@@ -29,14 +47,12 @@ public class BaleArmorItem extends Item {
                 }
                 default -> {}
             }
-
             return mutable.toImmutable();
         });
     }
 
-    // Remove enchantment glint from item
     @Override
-    public boolean isFoil(ItemStack itemStack) {
+    public boolean isFoil(@NonNull ItemStack itemStack) {
         return false;
     }
 }
