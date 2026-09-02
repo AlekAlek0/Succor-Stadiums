cde5746ef13dc512bde05f1d2ae72525e03d12d4
JaydenGarrett <swatj2472@gmail.com>
2026-09-01 20:07:51 -0600

    -Increased chance of farmbie blue spawning with fumblebringer fork to 2%

--- src/main/java/net/alek/succorstadiums/entity/monsters/FarmbieBlue.java
+++ src/main/java/net/alek/succorstadiums/entity/monsters/FarmbieBlue.java
@@ -1,11 +1,25 @@
 package net.alek.succorstadiums.entity.monsters;
 
+import net.alek.succorstadiums.item.ModItems;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.monster.zombie.Zombie;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.jspecify.annotations.NonNull;
 
-public class FarmbieBlue extends Farmbie{
+public class FarmbieBlue extends Farmbie {
     public FarmbieBlue(EntityType<? extends Zombie> type, Level level) {
         super(type, level);
+    }
+
+    @Override
+    protected void populateDefaultEquipmentSlots(@NonNull RandomSource random, @NonNull DifficultyInstance difficulty) {
+        super.populateDefaultEquipmentSlots(random, difficulty);
+        if (random.nextFloat() < 0.02F) {
+            this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(ModItems.FUMBLEBRINGER_FORK));
+        }
     }
 }
