ad4fab702423b9af6ab4d0c7cf3a789096c70e04
JaydenGarrett <swatj2472@gmail.com>
2026-09-12 15:53:03 -0600

    -Commented balearrowentityrenderer class

--- src/main/java/net/alek/succorstadiums/client/render/entity/items/BaleArrowEntityRenderer.java
+++ src/main/java/net/alek/succorstadiums/client/render/entity/items/BaleArrowEntityRenderer.java
@@ -1,27 +1,32 @@
 package net.alek.succorstadiums.client.render.entity.items;
 
-import net.alek.succorstadiums.SuccorStadiums;
-import net.alek.succorstadiums.entity.projectile.BaleArrowEntity;
-import net.minecraft.client.renderer.entity.ArrowRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.state.ArrowRenderState;
+import net.minecraft.client.renderer.entity.ArrowRenderer;
+
 import net.minecraft.resources.Identifier;
 import org.jspecify.annotations.NonNull;
 
+import net.alek.succorstadiums.entity.projectile.BaleArrowEntity;
+import net.alek.succorstadiums.SuccorStadiums;
+
+// BaleArrowEntityRenderer class
 public class BaleArrowEntityRenderer extends ArrowRenderer<BaleArrowEntity, ArrowRenderState> {
-
-    private static final Identifier TEXTURE =
-            Identifier.fromNamespaceAndPath(SuccorStadiums.MOD_ID, "textures/entity/projectiles/bale_arrow.png");
-
     public BaleArrowEntityRenderer(EntityRendererProvider.Context context) {
         super(context);
     }
 
+    // Create an identifier for the bale arrow entity texture
+    private static final Identifier TEXTURE =
+            Identifier.fromNamespaceAndPath(SuccorStadiums.MOD_ID, "textures/entity/projectiles/bale_arrow.png");
+
+    // Override the render state of a regular arrow with our custom arrow entity render state
     @Override
     public @NonNull ArrowRenderState createRenderState() {
         return new ArrowRenderState();
     }
 
+    // Override the texture location of a regular arrow with our custom arrow texture
     @Override
     protected @NonNull Identifier getTextureLocation(@NonNull ArrowRenderState renderState) {
         return TEXTURE;
