89b7d66ed168c4d92214f82b20b9ff3e03e07f9d
JaydenGarrett <swatj2472@gmail.com>
2026-09-18 15:46:39 -0600

    -Commented ModEntityAttributes

--- src/main/java/net/alek/succorstadiums/entity/ModEntityAttributes.java
+++ src/main/java/net/alek/succorstadiums/entity/ModEntityAttributes.java
@@ -1,21 +1,27 @@
 package net.alek.succorstadiums.entity;
 
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Creeper;
-import net.minecraft.world.entity.monster.cubemob.Slime;
 import net.minecraft.world.entity.monster.skeleton.Stray;
+import net.minecraft.world.entity.monster.cubemob.Slime;
 import net.minecraft.world.entity.monster.zombie.Zombie;
+import net.minecraft.world.entity.monster.Creeper;
 
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+
+// ModEntityAttributes class
 public class ModEntityAttributes {
 
+    // Register method
     public static void register() {
+
+        // Register banana slime entity attributes
         FabricDefaultAttributeRegistry.register(
                 ModEntityTypes.BANANA_SLIME,
                 Slime.createMobAttributes()
                         .add(Attributes.ATTACK_DAMAGE, 0D)
         );
 
+        // Register farmbie entity attributes
         FabricDefaultAttributeRegistry.register(
                 ModEntityTypes.FARMBIE,
                 Zombie.createAttributes()
@@ -27,6 +33,7 @@ public class ModEntityAttributes {
 
         );
 
+        // Register farmbie blue entity attributes
         FabricDefaultAttributeRegistry.register(
                 ModEntityTypes.FARMBIE_BLUE,
                 Zombie.createAttributes()
@@ -37,12 +44,14 @@ public class ModEntityAttributes {
                         .add(Attributes.ARMOR, 2D)
         );
 
+        // Register grass creeper entity attributes
         FabricDefaultAttributeRegistry.register(
                 ModEntityTypes.GRASS_CREEPER,
                 Creeper.createAttributes()
                         .add(Attributes.MAX_HEALTH, 10D)
         );
 
+        // Register skelcrow entity attributes
         FabricDefaultAttributeRegistry.register(
                 ModEntityTypes.SKELCROW,
                 Stray.createAttributes()
