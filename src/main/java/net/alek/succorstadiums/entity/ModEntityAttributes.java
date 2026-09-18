d515c61a0170b0da2d67531b488a82a528b41de9
JaydenGarrett <swatj2472@gmail.com>
2026-09-18 15:45:03 -0600

    -Increased farmbie blue movement speed to 0.25 from 0.24

--- src/main/java/net/alek/succorstadiums/entity/ModEntityAttributes.java
+++ src/main/java/net/alek/succorstadiums/entity/ModEntityAttributes.java
@@ -30,7 +30,7 @@ public class ModEntityAttributes {
         FabricDefaultAttributeRegistry.register(
                 ModEntityTypes.FARMBIE_BLUE,
                 Zombie.createAttributes()
-                        .add(Attributes.MOVEMENT_SPEED, 0.24D)
+                        .add(Attributes.MOVEMENT_SPEED, 0.25D)
                         .add(Attributes.ATTACK_DAMAGE, 2.5D)
                         .add(Attributes.ATTACK_SPEED, 0.5D)
                         .add(Attributes.MAX_HEALTH, 16.0D)
