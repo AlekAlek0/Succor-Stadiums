9220ce3c76bd66ebc1648ed011dc2fad31cf0a1f
JaydenGarrett <swatj2472@gmail.com>
2026-09-20 16:15:42 -0600

    -Removed chance of Farmbie spawning with fumblebringer forks

--- src/main/java/net/alek/succorstadiums/entity/monsters/Farmbie.java
+++ src/main/java/net/alek/succorstadiums/entity/monsters/Farmbie.java
@@ -1,26 +1,12 @@
 package net.alek.succorstadiums.entity.monsters;
 
-import net.alek.succorstadiums.item.ModItems;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.DifficultyInstance;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.monster.zombie.Zombie;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
-import org.jspecify.annotations.NonNull;
 
 public class Farmbie extends Zombie {
     public Farmbie(EntityType<? extends Zombie> type, Level level) {
         super(type, level);
-    }
-
-    @Override
-    protected void populateDefaultEquipmentSlots(@NonNull RandomSource random, @NonNull DifficultyInstance difficulty) {
-        super.populateDefaultEquipmentSlots(random, difficulty);
-        if (random.nextFloat() < 0.01F) {
-            this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(ModItems.FUMBLEBRINGER_FORK));
-        }
     }
 
 }
