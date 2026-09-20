5bfd5f5af93339c4eebd6a6f59c94773fe507af3
JaydenGarrett <swatj2472@gmail.com>
2026-09-20 17:03:31 -0600

    -Updated FarmbieBlue to have a fumblebringer fork in their offhand

--- src/main/java/net/alek/succorstadiums/entity/monsters/FarmbieBlue.java
+++ src/main/java/net/alek/succorstadiums/entity/monsters/FarmbieBlue.java
@@ -1,14 +1,16 @@
 package net.alek.succorstadiums.entity.monsters;
 
-import net.alek.succorstadiums.item.ModItems;
-import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.monster.zombie.Zombie;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.monster.zombie.Zombie;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
+
 import org.jspecify.annotations.NonNull;
+
+import net.alek.succorstadiums.item.ModItems;
 
 public class FarmbieBlue extends Farmbie {
     public FarmbieBlue(EntityType<? extends Zombie> type, Level level) {
@@ -17,9 +19,7 @@ public class FarmbieBlue extends Farmbie {
 
     @Override
     protected void populateDefaultEquipmentSlots(@NonNull RandomSource random, @NonNull DifficultyInstance difficulty) {
-        super.populateDefaultEquipmentSlots(random, difficulty);
-        if (random.nextFloat() < 0.02F) {
-            this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(ModItems.FUMBLEBRINGER_FORK));
-        }
+        this.setItemSlot(EquipmentSlot.OFFHAND, new ItemStack(ModItems.FUMBLEBRINGER_FORK));
+
     }
 }
