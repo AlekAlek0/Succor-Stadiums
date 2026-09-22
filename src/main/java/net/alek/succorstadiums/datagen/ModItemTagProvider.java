935e183cfacf19e6ebe39d75acdfa31a5068288c
JaydenGarrett <swatj2472@gmail.com>
2026-09-22 13:57:32 -0600

    -Added a new tag called coins for the emerald coin

--- src/main/java/net/alek/succorstadiums/datagen/ModItemTagProvider.java
+++ src/main/java/net/alek/succorstadiums/datagen/ModItemTagProvider.java
@@ -1,32 +1,40 @@
 package net.alek.succorstadiums.datagen;
 
-import net.alek.succorstadiums.SuccorStadiums;
-import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
-import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.Identifier;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.world.item.Item;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
-import org.jspecify.annotations.NonNull;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 
 import java.util.concurrent.CompletableFuture;
+import org.jspecify.annotations.NonNull;
 
+import net.alek.succorstadiums.SuccorStadiums;
+
+// ModItemTagProvider class
 public class ModItemTagProvider extends FabricTagsProvider.ItemTagsProvider {
     public ModItemTagProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
         super(output, registriesFuture);
     }
 
-    public static final TagKey<Item> BONE_DAGGERS = TagKey.create(
-            Registries.ITEM,
-            Identifier.fromNamespaceAndPath(SuccorStadiums.MOD_ID, "bone_daggers")
-    );
+    public static final TagKey<Item> COINS = TagKey.create(Registries.ITEM,
+            Identifier.fromNamespaceAndPath(SuccorStadiums.MOD_ID, "coins"));
 
+    public static final TagKey<Item> BONE_DAGGERS = TagKey.create(Registries.ITEM,
+            Identifier.fromNamespaceAndPath(SuccorStadiums.MOD_ID, "bone_daggers"));
 
     @Override
     protected void addTags(HolderLookup.@NonNull Provider wrapperLookup) {
+
+        builder(COINS)
+                .add(ResourceKey.create(Registries.ITEM,
+                        Identifier.fromNamespaceAndPath(SuccorStadiums.MOD_ID, "emerald_coin")));
+
         builder(ItemTags.SWORDS)
                 .add(ResourceKey.create(Registries.ITEM,
                         Identifier.fromNamespaceAndPath(SuccorStadiums.MOD_ID, "sword_of_the_forest")))
