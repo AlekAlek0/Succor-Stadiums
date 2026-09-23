391d0a96be2457cd784674cc56e500734b8560dd
JaydenGarrett <swatj2472@gmail.com>
2026-09-23 00:50:05 -0600

    -Added plains coin pouch

---
+++ src/main/java/net/alek/succorstadiums/item/equipment/pouch/PouchSettingsHelper.java
@@ -0,0 +1,24 @@
+package net.alek.succorstadiums.item.equipment.pouch;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.resources.Identifier;
+
+import net.alek.succorstadiums.attachments.ModAttachments;
+
+// PouchSettingsHelper class
+public final class PouchSettingsHelper {
+
+    private PouchSettingsHelper() {}
+
+    // Check if auto deposit is enabled by fetching the players pouch settings mod attachment
+    public static boolean isAutoDepositEnabled(Player player, Identifier pouchTypeId) {
+        return player.getAttachedOrCreate(ModAttachments.POUCH_SETTINGS, PouchSettingsData::new)
+                .isAutoDepositEnabled(pouchTypeId);
+    }
+
+    // Set the players auto deposit setting
+    public static void setAutoDepositEnabled(Player player, Identifier pouchTypeId, boolean enabled) {
+        PouchSettingsData data = player.getAttachedOrCreate(ModAttachments.POUCH_SETTINGS, PouchSettingsData::new);
+        player.setAttached(ModAttachments.POUCH_SETTINGS, data.withAutoDeposit(pouchTypeId, enabled));
+    }
+}
