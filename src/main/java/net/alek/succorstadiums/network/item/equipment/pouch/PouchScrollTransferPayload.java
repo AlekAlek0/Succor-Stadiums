8cddc95bc162b358569a6d1cb0aa90f3954f2eef
JaydenGarrett <swatj2472@gmail.com>
2026-09-23 19:05:03 -0600

    -Added plains coin pouch

--- src/main/java/net/alek/succorstadiums/network/item/equipment/pouch/PouchScrollTransferPayload.java
+++ src/main/java/net/alek/succorstadiums/network/item/equipment/pouch/PouchScrollTransferPayload.java
@@ -11,14 +11,17 @@ import org.jspecify.annotations.NonNull;
 // PouchScrollTransferPayload class
 public record PouchScrollTransferPayload(boolean deposit) implements CustomPacketPayload {
 
+    // Create a type for the payload
     public static final Type<PouchScrollTransferPayload> TYPE =
             new Type<>(Identifier.fromNamespaceAndPath("succorstadiums", "pouch_scroll_transfer"));
 
+    // Create a stream codec for the PouchScrollTransferPayload
     public static final StreamCodec<RegistryFriendlyByteBuf, PouchScrollTransferPayload> CODEC = StreamCodec.composite(
             ByteBufCodecs.BOOL, PouchScrollTransferPayload::deposit,
             PouchScrollTransferPayload::new
     );
 
+    // Override the type of our custom packet with our type
     @Override
     public @NonNull Type<? extends CustomPacketPayload> type() {
         return TYPE;
